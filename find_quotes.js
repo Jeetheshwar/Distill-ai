5d64d6f858c443dcb57e008313aa05b6ea9f5ea1
Jeetheshwar <jeetheshwaraalam7@gmail.com>
2026-06-13 09:37:44 +0530

    chore: remove scratch files causing lint errors

--- find_quotes.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/app/dashboard/pipelines/page.tsx', 'utf8');
-const lines = content.split('\n');
-lines.forEach((l, i) => {
-  if (l.includes('"') || l.includes("'")) {
-    console.log((i+1) + ': ' + l.trim());
-  }
-});
